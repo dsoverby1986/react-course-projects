ec4b0caec86426a3c2ae4a9797d59298862b3dad
shaneoverby <soverby@core-techs.net>
2017-12-15 00:08:50 -0500

    nesting components; using component class props; mapping to render multiple components of the same type

--- src/app.js
+++ src/app.js
@@ -1,9 +1,26 @@
+class IndecisionApp extends React.Component {
+    render() {
+        const title = 'Indecision App';
+        const subtitle = 'Put your life in the hands of a computer';
+        const options = ['Thing one', 'Thing two', 'Thing four'];
+
+        return (
+            <div>
+                <Header title={title} subtitle={subtitle} />
+                <Action />
+                <Options options={options} />
+                <AddOption />
+            </div>
+        );
+    }
+}
+
 class Header extends React.Component {
     render() {
         return (
             <div>
-                <h1>Indecision</h1>
-                <h2>Put your life in the hands of a computer</h2>
+                <h1>{this.props.title}</h1>
+                <h2>{this.props.subtitle}</h2>
             </div>
         );
     }
@@ -24,11 +41,19 @@ class Options extends React.Component {
         return (
             <div>
                 <ul>
-                    <li>Option 1</li>
-                    <li>Option 2</li>
-                    <li>Option 3</li>
+                    {
+                        this.props.options.map((option) => <Option key={option} optionText={option} />)
+                    }
                 </ul>
             </div>
+        );
+    }
+}
+
+class Option extends React.Component {
+    render(){
+        return (
+            <li>{this.props.optionText}</li>
         );
     }
 }
@@ -44,13 +69,4 @@ class AddOption extends React.Component {
     }
 }
 
-const jsx = (
-    <div>
-        <Header />
-        <Action />
-        <Options />
-        <AddOption />
-    </div>
-);
-
-ReactDOM.render(jsx, document.getElementById('app'));
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
